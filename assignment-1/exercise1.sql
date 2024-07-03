be89ca2634b78addd4f45670291415d74105a498
bhuwan <bhubanpaudel79@gmail.com>
2024-07-03 14:05:50 +0545

    make first assignment uppercase

--- assignment-1/exercise1.sql
+++ assignment-1/exercise1.sql
@@ -1,63 +1,72 @@
--- Create table for products 
- create table Products(
- product_id int primary key  ,
- product_name varchar(255) not null,
-category varchar(255),
- price int not null
+-- CREATE TABLE FOR PRODUCTS 
+ CREATE TABLE PRODUCTS(
+ PRODUCT_ID INT PRIMARY KEY  ,
+ PRODUCT_NAME VARCHAR(255) NOT NULL,
+CATEGORY VARCHAR(255),
+ PRICE INT NOT NULL
+)
+--  TABLE FOR ORDERS
+CREATE TABLE ORDERS(
+ORDER_ID INT PRIMARY KEY, 
+CUSTOMER_NAME VARCHAR(255) NOT NULL, 
+PRODUCT_ID INT , 
+QUANTITY FLOAT(8) , 
+ORDER_DATE DATE,
+FOREIGN KEY(PRODUCT_ID)
+REFERENCES PRODUCTS(PRODUCT_ID)
 );
 
-
---  insert into products
-INSERT INTO Products (product_id, product_name, category, price)
+--  INSERT INTO PRODUCTS
+INSERT INTO PRODUCTS (PRODUCT_ID, PRODUCT_NAME, CATEGORY, PRICE)
 VALUES
-    (1, 'Product A', 'Category 1', 10),
-    (2, 'Product B', 'Category 2', 20),
-    (3, 'Product C', 'Category 1', 30);
+    (1, 'PRODUCT A', 'CATEGORY 1', 10),
+    (2, 'PRODUCT B', 'CATEGORY 2', 20),
+    (3, 'PRODUCT C', 'CATEGORY 1', 30);
 
-	--  insert into orders 
-INSERT INTO Orders (order_id, customer_name, product_id, quantity, order_date)
+	--  INSERT INTO ORDERS 
+INSERT INTO ORDERS (ORDER_ID, CUSTOMER_NAME, PRODUCT_ID, QUANTITY, ORDER_DATE)
 VALUES
-    (1, 'Customer 1', 1, 2, '2024-06-01'),
-    (2, 'Customer 2', 2, 3, '2024-06-02'),
-    (3, 'Customer 1', 3, 1, '2024-06-03'),
-    (4, 'Customer 3', 1, 4, '2024-06-04'),
-    (5, 'Customer 2', 3, 2, '2024-06-05'),
-    (6, 'Customer 3', 2, 1, '2024-06-06');
+    (1, 'CUSTOMER 1', 1, 2, '2024-06-01'),
+    (2, 'CUSTOMER 2', 2, 3, '2024-06-02'),
+    (3, 'CUSTOMER 1', 3, 1, '2024-06-03'),
+    (4, 'CUSTOMER 3', 1, 4, '2024-06-04'),
+    (5, 'CUSTOMER 2', 3, 2, '2024-06-05'),
+    (6, 'CUSTOMER 3', 2, 1, '2024-06-06');
 
 
--- Update 
-update orders
-set customer_name='bob'
-where product_id = 1;
+-- UPDATE 
+UPDATE ORDERS
+SET CUSTOMER_NAME='BOB'
+WHERE PRODUCT_ID = 1;
 
---  delete 
-Delete from orders
-Where order_id = 5;
+--  DELETE 
+DELETE FROM ORDERS
+WHERE ORDER_ID = 5;
 
---  Q->Calculate the total quantity ordered for each product category in the orders table.
+--  Q->CALCULATE THE TOTAL QUANTITY ORDERED FOR EACH PRODUCT CATEGORY IN THE ORDERS TABLE.
 
-select Products.category , sum(Orders.quantity) as total_quantity_ordred
-from Products
-inner join Orders on  Products.product_id = Orders.product_id
-group by Products.category;
-
-
---  without join 
-SELECT 
-    Products.category,
-    SUM(Orders.quantity) AS total_quantity_ordered FROM 
-    Products, Orders  
-WHERE 
-    Products.product_id = Orders.product_id  
-GROUP BY 
-    Products.category;
+SELECT PRODUCTS.CATEGORY , SUM(ORDERS.QUANTITY) AS TOTAL_QUANTITY_ORDRED
+FROM PRODUCTS
+INNER JOIN ORDERS ON  PRODUCTS.PRODUCT_ID = ORDERS.PRODUCT_ID
+GROUP BY PRODUCTS.CATEGORY;
 
 
+--  WITHOUT JOIN 
+ SELECT 
+    PRODUCTS.CATEGORY,
+    SUM(ORDERS.QUANTITY) AS TOTAL_QUANTITY_ORDERED FROM 
+    PRODUCTS, ORDERS  
+ WHERE 
+    PRODUCTS.PRODUCT_ID = ORDERS.PRODUCT_ID  
+ GROUP BY 
+    PRODUCTS.CATEGORY;
 
---  Q -> Find categories where the total number of products ordered is greater than 5. 
 
-select Products.category , sum(Orders.quantity) as total_quantity_ordred
-from Products
-inner join Orders on  Products.product_id = Orders.product_id
-group by Products.category
-having sum(Orders.quantity)>5;
+
+--  Q -> FIND CATEGORIES WHERE THE TOTAL NUMBER OF PRODUCTS ORDERED IS GREATER THAN 5. 
+
+SELECT PRODUCTS.CATEGORY , SUM(ORDERS.QUANTITY) AS TOTAL_QUANTITY_ORDRED
+FROM PRODUCTS
+INNER JOIN ORDERS ON  PRODUCTS.PRODUCT_ID = ORDERS.PRODUCT_ID
+GROUP BY PRODUCTS.CATEGORY
+HAVING SUM(ORDERS.QUANTITY)>5;
